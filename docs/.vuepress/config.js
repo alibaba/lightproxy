2724b593561defc1375aff0ed9d72e32641d3876
jinxin.sjx <jinxin.sjx@alibaba-inc.com>
2020-01-12 19:41:59 +0800

    update gitter

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,27 +2,55 @@ module.exports = {
     title: 'LightProxy',
     base: '/lightproxy/',
     description: '基于 whistle 的代理调试软件',
+    locales: {
+      '/': {
+        lang: 'zh-CN',
+        selectText: '选择语言',
+        label: '简体中文',
+      },
+      '/en/': {
+        lang: 'en-US',
+        selectText: 'Languages',
+        label: 'English',
+        ariaLabel: 'Languages'
+      },
+    },
     themeConfig: {
         repo: 'alibaba/lightproxy',
         repoLabel: 'Github',
+        
     },
     head: [
         ['meta', { name: 'referrer', content: 'no-referrer' }]
     ],
     plugins: [
       [
-        'vuepress-plugin-yuque', {
+        '@xcodebuild/vuepress-plugin-yuque', {
           repoUrl: 'https://www.yuque.com/lp/doc/',
           html: true,
           home: {
-
-            actionText: '现在开始 →',
+            actionText: 'Quick Start →',
             actionLink: '/quick-start.html',
             heroImage: 'https://img.alicdn.com/tfs/TB19.p_rXP7gK0jSZFjXXc5aXXa-512-512.png',
-            footer: `Copyright © Alibaba IFE 团队`
+            footer: `Copyright © Alibaba IFE Team`
+          },
+          multipleLanguage: {
+            '/en': 'https://www.yuque.com/lp/doc-en/',
           }
         }
       ],
+      // [
+      //   '@xcodebuild/vuepress-plugin-yuque', {
+      //     repoUrl: 'https://www.yuque.com/lp/doc-en/',
+      //     html: true,
+      //     home: {
+      //       actionText: 'Quick Start →',
+      //       actionLink: '/quick-start.html',
+      //       heroImage: 'https://img.alicdn.com/tfs/TB19.p_rXP7gK0jSZFjXXc5aXXa-512-512.png',
+      //       footer: `Copyright © Alibaba IFE Team`
+      //     },
+      //   }
+      // ],
       [
         '@vuepress/google-analytics',
         {
