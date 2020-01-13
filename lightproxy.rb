244fc0f0a91e53eaf15dc67202f3d260aed5797b
jinxin.sjx <jinxin.sjx@alibaba-inc.com>
2020-01-14 00:29:11 +0800

    remove appcast

--- lightproxy.rb
+++ lightproxy.rb
@@ -3,7 +3,6 @@ cask 'lightproxy' do
   sha256 'dad8d44ee3b092f046a767676d0131df8d70ab930b5620237dbbe8c7ff102d8f'
 
   url "https://gw.alipayobjects.com/os/LightProxy/ec832c1b-6f7e-49c6-a129-9f459f29e4ba/LightProxy.dmg"
-  appcast ''
   name 'LightProxy'
   homepage 'https://github.com/alibaba/lightproxy'
 
