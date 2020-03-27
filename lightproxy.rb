0766e85dbfe3bccd5a1815b87d5c52e6fb0e6101
jinxin.sjx <jinxin.sjx@alibaba-inc.com>
2020-03-27 14:47:28 +0800

    release 1.1.10

--- lightproxy.rb
+++ lightproxy.rb
@@ -1,8 +1,8 @@
 cask 'lightproxy' do
-  version '1.1.8'
-  sha256 '287cfc258692bf89acb450111b0c7f1ac63ccc66f82f9b13982cf6a1d29cd27c'
+  version '1.1.10'
+  sha256 'da31062bc4112332bb745c6199fb681a5eb533c0475fe6b44a46f56c558a2b41'
   # gw.alipayobjects.com/os/LightProxy was verified as official when first introduced to the cask
-  url "https://gw.alipayobjects.com/os/LightProxy/7372dabd-cb4e-43e5-98a2-e7f22785dc76/LightProxy.dmg"
+  url "https://gw.alipayobjects.com/os/LightProxy/978ed8ce-b710-4a00-b348-901297fd39ae/LightProxy.dmg"
   name 'LightProxy'
   homepage 'https://github.com/alibaba/lightproxy'
 
