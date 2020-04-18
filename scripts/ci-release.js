5ab4baac316979c9c6fc72794c25b08d3cd2a133
xcodebuild <me@xcodebuild.com>
2020-04-18 16:55:35 +0800

    decode env var

--- scripts/ci-release.js
+++ scripts/ci-release.js
@@ -1,11 +1,12 @@
 const path = require('path');
-const cp = require('child_process');
 const shell = require('shelljs');
+const fs = require('fs');
 
 const {
     TRAVIS_PULL_REQUEST,
     TRAVIS_COMMIT,
     SSH_KEY,
+    HOME,
 } = process.env;
 
 const commitId = TRAVIS_COMMIT.slice(0, 6);
@@ -15,7 +16,10 @@ const RELEASE_DIR = path.join(__dirname, '../release');
 console.log('pr', TRAVIS_PULL_REQUEST);
 
 if (TRAVIS_PULL_REQUEST !== 'false') {
-    console.log(shell.exec(`echo ${SSH_KEY} > ~/.ssh/id_rsa`));
+    const sshFile = path.join(HOME, './.ssh/id_rsa');
+    const sshKeyContent = decodeURIComponent(SSH_KEY);
+    fs.writeFileSync(sshFile, sshKeyContent, 'utf-8');
+    
     console.log(shell.exec(`cd ${RELEASE_DIR} && scp *.exe xcodebuild@sourceforge.net:/home/frs/project/lightproxy/build/Lightproxy-${commitId}.exe`));
 }
 console.log('done');
