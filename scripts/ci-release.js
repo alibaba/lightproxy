d67bfa1729378b579c1f6b06c3e9a4494448a7bf
xcodebuild <me@xcodebuild.com>
2020-04-18 17:33:54 +0800

    fix sourceforge

--- scripts/ci-release.js
+++ scripts/ci-release.js
@@ -7,6 +7,7 @@ const {
     TRAVIS_COMMIT,
     SSH_KEY,
     HOME,
+    BOT_TOKEN,
 } = process.env;
 
 const commitId = TRAVIS_COMMIT.slice(0, 6);
@@ -20,6 +21,8 @@ if (TRAVIS_PULL_REQUEST !== 'false') {
     const sshKeyContent = decodeURIComponent(SSH_KEY);
     fs.writeFileSync(sshFile, sshKeyContent, 'utf-8');
     
-    console.log(shell.exec(`cd ${RELEASE_DIR} && scp *.exe xcodebuild@sourceforge.net:/home/frs/project/lightproxy/build/Lightproxy-${commitId}.exe`));
+    console.log(shell.exec(`cd ${RELEASE_DIR} && scp *.exe xcodebuild@frs.sourceforge.net:/home/frs/project/lightproxy/build/Lightproxy-${commitId}.exe`));
+
+    // console.log(`curl -s -H "Authorization: token ${BOT_TOKEN}" -X POST -d '{"body": "Thanks for pull request, build for this pr is ready, you can test it from here"}' "https://api.github.com/repos/alibaba/lightproxy/issues/${TRAVIS_PULL_REQUEST}/comments"`);
 }
 console.log('done');
