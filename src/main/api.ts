90d63f49b68a8dff222565d940dd658e7711a0ce
xcodebuild <jinxin.sjx@alibaba-inc.com>
2020-04-29 20:00:21 +0800

    try to fix #113

--- src/main/api.ts
+++ src/main/api.ts
@@ -58,6 +58,8 @@ require(decodeURIComponent('${modulePath}'));`;
                 `const code = decodeURIComponent("${encodeURIComponent(nodeScript)}");console.log(code);eval(code);`,
                 '--tls-min-v1.0',
                 '--max-http-header-size=256000',
+                '--insecure-http-parser',
+                '--http-parser=legacy'
             ],
             {
                 env: {
