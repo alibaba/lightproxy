a6415a5ca3287469c11fd191a071c138e5a8228d
xcodebuild <me@xcodebuild.com>
2020-05-09 23:49:36 +0800

    give a try

--- src/main/updater.ts
+++ src/main/updater.ts
@@ -2,6 +2,9 @@ import Store from 'electron-store';
 import { sendMessageToWindow } from './utils';
 import logger from 'electron-log';
 import { autoUpdater } from 'electron-updater';
+import {
+    app,
+} from 'electron';
 
 let updaterInited = false;
 
@@ -41,15 +44,27 @@ export async function checkUpdater() {
         const store = new Store();
         const settings = store.get('settings') || {};
         updateChannel = settings.updateChannel || 'stable';
+        updateChannel = updateChannel === 'stable' ? 'latest' : 'beta';
     } catch (e) {}
 
     try {
-        const url =
-            updateChannel === 'stable'
-                ? 'https://gw.alipayobjects.com/os/LightProxy/simple-updater-release.json'
-                : 'https://gw.alipayobjects.com/os/LightProxy/simple-updater-beta-release.json';
+        // const url =
+        //     updateChannel === 'stable'
+        //         ? 'https://gw.alipayobjects.com/os/LightProxy/simple-updater-release.json'
+        //         : 'https://gw.alipayobjects.com/os/LightProxy/simple-updater-beta-release.json';
+
+        autoUpdater.channel = 'beta';
+        autoUpdater.setFeedURL({
+            url: 'https://release.lightproxy.org/release',
+            provider: 'generic',
+        });
 
         initUpdater();
+        Object.defineProperty(app, 'isPackaged', {
+            get() {
+                return true;
+            }
+        });
         autoUpdater.checkForUpdatesAndNotify();
     } catch (e) {
         logger.error(e);
