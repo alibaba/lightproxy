c454028a6f09cf7436d31099a385c68015f2d3bb
jinxin.sjx <jinxin.sjx@alibaba-inc.com>
2020-05-11 08:20:23 +0800

    purge cache after depoly

--- lightproxy.rb
+++ lightproxy.rb
@@ -1,8 +1,8 @@
 cask 'lightproxy' do
-  version '1.1.13'
-  sha256 '56c82500f8e8321dcf7418e0b4d543982b07e549c982862ea8e71276811228f6'
+  version '1.1.14'
+  sha256 '4530ecd308c2b7111bc6e5e58f412e8c31575970c4b697b935ee9226484da433'
   # gw.alipayobjects.com/os/LightProxy was verified as official when first introduced to the cask
-  url "https://gw.alipayobjects.com/os/LightProxy/c0a6584d-c3de-4e16-803d-bfc6b3746024/LightProxy.dmg"
+  url "https://gw.alipayobjects.com/os/LightProxy/281e16c2-affb-473a-9984-c43375a1709c/LightProxy.dmg"
   name 'LightProxy'
   homepage 'https://github.com/alibaba/lightproxy'
 
