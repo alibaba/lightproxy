17f3ef9d67895990d031fc5c77585f2f12bb1b6c
ahonn <ahonn95@outlook.com>
2020-09-11 00:52:01 +0800

    build: setting up purge css

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,20 @@
+// https://tailwindcss.com/docs/controlling-file-size#setting-up-purge-css-manually
+const purgecss = require('@fullhuman/postcss-purgecss')({
+  content: ['./src/**/*.jsx', './src/**/*.js'],
+
+  defaultExtractor: (content) => {
+    const broadMatches = content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || [];
+    const innerMatches = content.match(/[^<>"'`\s.()]*[^<>"'`\s.():]/g) || [];
+
+    return broadMatches.concat(innerMatches);
+  },
+});
+
 module.exports = {
-    plugins: [
-      require('tailwindcss'),
-    ]
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+    ...(process.env.NODE_ENV === 'production' ? [purgecss] : []),
+  ],
 };
+
